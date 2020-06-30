6420e3da05bb72a50ff154730b8d011bb0a2a787
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-06-30 15:19:18 -0600

    Import-Module added

--- PowerShell/JumpCloud Module/Tests/Public/Authentication/Update-JCModule.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Authentication/Update-JCModule.Tests.ps1
@@ -1,6 +1,7 @@
 Describe -Tag:('JCModule') 'Test for Update-JCModule' {
     It ('Where the local version number has been updated to match the PowerShell gallery version number') {
         Install-Module -Name:('JumpCloud') -RequiredVersion:('1.0.0') -Scope:('CurrentUser') -Force
+        Import-Module -Name:('JumpCloud') -Version:('1.0.0')
         $InitialModule = Get-Module -Name:('JumpCloud') -All | Where-Object { $_.Version -eq '1.0.0' }
         $PowerShellGalleryModule = Find-Module -Name:('JumpCloud') -ErrorAction:('Ignore')
         $LocalModulePre = Get-Module -Name:('JumpCloud')
