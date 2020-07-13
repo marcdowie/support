87ce18a9529c40cf93c1b72dca921f4adbd10303
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-07-13 11:41:17 -0600

    initial

--- AWS/Serverless/Events/get-jcevents.py
+++ AWS/Serverless/Events/get-jcevents.py
@@ -1,10 +1,13 @@
 import requests, datetime, json, boto3, os, gzip
 
 def jc_events(event, context):
-    jcapikey = os.environ['JCAPIKEY']
-    incrementType = os.environ['incrementType']
-    incrementAmount = int(os.environ['incrementAmount'])
-    bucketName = os.environ['BucketName']
+    #jcapikey = os.environ['JCAPIKEY']
+    #incrementType = os.environ['incrementType']
+    #incrementAmount = int(os.environ['incrementAmount'])
+    #bucketName = os.environ['BucketName']
+    jcapikey = ""
+    incrementType = "days"
+    incrementAmount = 14
 
     now = datetime.datetime.utcnow()
 
@@ -26,21 +29,26 @@ def jc_events(event, context):
     start_date = start_dt.isoformat("T") + "Z"
     end_date = now.isoformat("T") + "Z"
 
-    fileStartDate = datetime.datetime.strftime(start_dt, "%m-%d-%YT%H-%M-%SZ")
-    fileEndDate = datetime.datetime.strftime(now, "%m-%d-%YT%H-%M-%SZ")
-    outfileName = "jcevents" + fileEndDate + "_" + fileStartDate + ".json.gz"
+    #fileStartDate = datetime.datetime.strftime(start_dt, "%m-%d-%YT%H-%M-%SZ")
+    #fileEndDate = datetime.datetime.strftime(now, "%m-%d-%YT%H-%M-%SZ")
+    #outfileName = "jcevents" + fileEndDate + "_" + fileStartDate + ".json.gz"
 
-    url = "https://events.jumpcloud.com/events"
+    url = "https://api.jumpcloud.com/insights/directory/v1/events"
 
-    payload = "startDate=" + start_date + "&endDate=" + end_date
+    body = {
+        'service': ["all"],
+        'start_time': start_date,
+        'end_time': end_date,
+        "limit": 10
+    }
     headers = {
         'x-api-key': jcapikey,
         'content-type': "application/json",
         }
 
     try:
-        response = requests.request("GET", url, params=payload, headers=headers)
-        response = json.loads(response.text)
+        response = requests.post(url, json=body, headers=headers)
+        responseBody = json.loads(response.text)
     except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
         raise Exception(e)
         exit(1)
@@ -49,9 +57,20 @@ def jc_events(event, context):
         raise Exception("There have been no events in the last {0} {1}.".format(incrementAmount, incrementType))
         return 
 
-    gzOutfile = gzip.GzipFile(filename="/tmp/" + outfileName, mode="w", compresslevel=9)
-    gzOutfile.write(json.dumps(response, indent=4).encode("UTF-8"))
-    gzOutfile.close()
+    data = json.dumps(responseBody, indent=4).encode("UTF-8")
 
-    s3 = boto3.client('s3')
-    s3.upload_file("/tmp/" + outfileName, bucketName, outfileName)
+    while (response.headers["X-Result-Count"] >= response.headers["X-Limit"]):
+        body["search_after"] = response.headers["X-Search_After"]
+        try:
+            response = requests.post(url, json=body, headers=headers)
+            responseBody = json.loads(response.text)
+            data = data + json.dumps(responseBody, indent=4).encode("UTF-8")
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
+            raise Exception(e)
+        
+    #gzOutfile = gzip.GzipFile(filename="/tmp/" + outfileName, mode="w", compresslevel=9)
+    #gzOutfile.write(data)
+    #gzOutfile.close()
+
+    #s3 = boto3.client('s3')
+    #s3.upload_file("/tmp/" + outfileName, bucketName, outfileName)
